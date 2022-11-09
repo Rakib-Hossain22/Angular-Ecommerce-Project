589bcdffc6822da74c3b0eec8093e8d98d722230
RakibFS <md.rakib@selise.ch>
2022-11-09 19:30:07 +0600

    Seller Login API Integration

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,31 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SignUp } from '../data-type';
+import { Login, SignUp } from '../data-type';
 import { SellerService } from '../services/seller.service';
 
 @Component({
     selector: 'app-seller-auth',
     templateUrl: './seller-auth.component.html',
-    styleUrls: ['./seller-auth.component.scss']
+    styleUrls: ['./seller-auth.component.scss'],
 })
 export class SellerAuthComponent implements OnInit {
-
     showLogin = false;
-    constructor(
-        private seller: SellerService,
-        private router: Router
-    ) { }
+    authError: string = 'false';
+    constructor(private seller: SellerService, private router: Router) { }
 
     ngOnInit(): void {
         this.seller.reloadSeller();
     }
-    
+
     signUp(data: SignUp): void {
+        console.log(data);
         this.seller.userSignup(data);
     }
 
-    login(data: SignUp): void {
-        console.log(data);
+    login(data: Login): void {
+        this.authError = "";
+        // console.log(data);
+        this.seller.userLogin(data);
+        this.seller.isLoginError.subscribe((isError) => {
+            if (isError) {
+                this.authError = "Email or Password in not Correct";
+            }
+        })
     }
 
     openLogin() {
@@ -35,5 +40,4 @@ export class SellerAuthComponent implements OnInit {
     openSignUp() {
         this.showLogin = false;
     }
-
 }
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,28 +1,47 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { SignUp } from '../data-type';
+import { Login, SignUp } from '../data-type';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class SellerService {
-  isSellerLoggedIn = new BehaviorSubject<boolean>(false);
-  constructor(private http: HttpClient, private router: Router) {}
+    isSellerLoggedIn = new BehaviorSubject<boolean>(false);
+    isLoginError = new EventEmitter<boolean>(false);
+    constructor(private http: HttpClient, private router: Router) { }
 
     userSignup(data: SignUp) {
         this.http
-        .post('http://localhost:3000/seller', data, { observe: 'response' })
-        .subscribe((result) => {
-            this.isSellerLoggedIn.next(true);
-            localStorage.setItem('seller', JSON.stringify(result.body));
-            this.router.navigate(['seller-home']);
-            console.log('Result', result);
-        });
-  }
+            .post('http://localhost:3000/seller', data, { observe: 'response' })
+            .subscribe((result) => {
+                localStorage.setItem('seller', JSON.stringify(result.body));
+                this.router.navigate(['seller-home']);
+            });
+    }
 
     reloadSeller() {
-        
+        if (localStorage.getItem('seller')) {
+            this.isSellerLoggedIn.next(true);
+            this.router.navigate(['seller-home']);
+        }
+    }
+
+    userLogin(data: Login) {
+        console.log(data);
+        this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,
+            { observe: 'response' }
+        ).subscribe((result: any) => {
+            console.log(result);
+            if (result && result.body && result.body.length) {
+                console.log("User Login Successfull");
+                localStorage.setItem('seller', JSON.stringify(result.body));
+                this.router.navigate(['seller-home']);
+            } else {
+                console.log("User Login Failed");
+                this.isLoginError.emit(true);
+            }
+        });
     }
 }
