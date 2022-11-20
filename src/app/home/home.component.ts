e6edc2ad1f3177e3a07464d57fdce7c7b312498c
RakibFS <md.rakib@selise.ch>
2022-11-20 19:28:47 +0600

    Display Product in Bootstrap Carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { product } from './../data-type';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
     selector: 'app-home',
@@ -7,12 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-    images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+    popularProducts: undefined | product[];
 
-
-    constructor() { }
+    constructor(private product: ProductService) { }
 
     ngOnInit(): void {
+        this.product.popularProducts().subscribe((data) => {
+            console.log(data);
+            this.popularProducts = data;
+        })
     }
 
 }
