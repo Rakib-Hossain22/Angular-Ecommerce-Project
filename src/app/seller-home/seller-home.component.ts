213a86b4e3602fe8970c60142a804a2f57c359a9
RakibFS <md.rakib@selise.ch>
2022-11-20 15:17:30 +0600

    UI for Product Update

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,7 +1,7 @@
 import { product } from './../data-type';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
 @Component({
     selector: 'app-seller-home',
     templateUrl: './seller-home.component.html',
@@ -11,7 +11,7 @@ export class SellerHomeComponent implements OnInit {
     productList: undefined | product[];
     productMessage: undefined | string;
     icon = faTrash;
-    
+    EditIcon = faEdit;
     constructor(private product: ProductService) { }
 
     ngOnInit(): void {
