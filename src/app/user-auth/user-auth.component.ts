512d638dd34731d533c7d1f627579610bab46383
RakibFS <md.rakib@selise.ch>
2022-11-28 12:47:31 +0600

    Sign Up Form in Angular

---
+++ src/app/user-auth/user-auth.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { SignUp } from '../data-type';
+
+@Component({
+    selector: 'app-user-auth',
+    templateUrl: './user-auth.component.html',
+    styleUrls: ['./user-auth.component.scss']
+})
+export class UserAuthComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {}
+
+    signUp(data: SignUp) {
+        console.log(data);
+    }
+
+}
