6e94e3342269c7d62393751aed4374de857cafa1
Md Rakib Hossain <rakibuits1059@gmail.com>
2022-12-06 14:19:20 +0600

    Store Local Storage Cart To DB

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,6 +1,8 @@
+import { cart, product } from './../data-type';
 import { Component, OnInit } from '@angular/core';
 import { Login, SignUp } from '../data-type';
 import { UserService } from '../services/user.service';
+import { ProductService } from '../services/product.service';
 
 @Component({
     selector: 'app-user-auth',
@@ -10,7 +12,10 @@ import { UserService } from '../services/user.service';
 export class UserAuthComponent implements OnInit {
     showLogin: boolean = true;
     authError: string = '';
-    constructor(private user: UserService) { }
+    constructor(
+        private user: UserService,
+        private product: ProductService
+    ) { }
 
     ngOnInit(): void {
         this.user.userAuthReload();
@@ -23,8 +28,11 @@ export class UserAuthComponent implements OnInit {
     login(data: Login) {
         this.user.userLogin(data);
         this.user.invalidUserAuth.subscribe((result) => {
+            console.log(result);
             if (result) {
-                this.authError = "Please enter valid user details";
+                this.authError = "User Not Found!";
+            } else {
+                this.localCartToRemoteCart();
             }
         });
     }
@@ -35,6 +43,34 @@ export class UserAuthComponent implements OnInit {
 
     openSignUp() {
         this.showLogin = false;
+    }
+
+    localCartToRemoteCart() {
+        let data = localStorage.getItem('localCart');
+        if (data) {
+            let cartDataList = JSON.parse(data);
+            let user = localStorage.getItem('user');
+            let userId = user && JSON.parse(user).id;
+
+            cartDataList.forEach((product: product, index: number) => {
+                let cartData: cart = {
+                    ...product,
+                    productId: product.id,
+                    userId
+                };
+                delete cartData.id;
+                setTimeout(() => {
+                    this.product.addToCart(cartData).subscribe((result) => {
+                        if (result) {
+                            console.log("Item Stored in DB");
+                        }
+                    });
+                    if (cartDataList.length === index + 1) {
+                        localStorage.removeItem('localCart');
+                    }
+                }, 500);
+            });
+        }
     }
 
 }
